0e76ef4abf417d8d5df42d7c4cfa0c541ac737d4
André Negreiros <112659872+Andrengr0@users.noreply.github.com>
2023-05-27 18:12:45 -0300

    add quizz

--- js/functionsquizz.js
+++ js/functionsquizz.js
@@ -11,7 +11,6 @@ $(()=>{
             $('.c1_3').css('background-color','#87ff93');
         }
 
-
         idInputPg2 = $('input[name=pg2]:checked').val();
         if(idInputPg2 == 2){
             acertos += 1;
@@ -21,26 +20,52 @@ $(()=>{
             $('.c2_2').css('background-color','#87ff93');
         }
 
-        let porc_acertos = (acertos / 2) * 100;
+        idInputPg3 = $('input[name=pg3]:checked').val();
+        if(idInputPg3 == 4){
+            acertos += 1;
+            $('.c3_4').css('background-color','#87ff93');
+        }else{
+            $('.c3_'+idInputPg3).css('background-color','#ff9595');
+            $('.c3_4').css('background-color','#87ff93');
+        }
+
+        idInputPg4 = $('input[name=pg4]:checked').val();
+        if(idInputPg4 == 1){
+            acertos += 1;
+            $('.c4_1').css('background-color','#87ff93');
+        }else{
+            $('.c4_'+idInputPg4).css('background-color','#ff9595');
+            $('.c4_1').css('background-color','#87ff93');
+        }
+
+        idInputPg5 = $('input[name=pg5]:checked').val();
+        if(idInputPg5 == 5){
+            acertos += 1;
+            $('.c5_5').css('background-color','#87ff93');
+        }else{
+            $('.c5_'+idInputPg5).css('background-color','#ff9595');
+            $('.c5_5').css('background-color','#87ff93');
+        }
+
+        let porc_acertos = (acertos / 5) * 100;
 
         var feedback;
         if(porc_acertos <= 30){
-            feedback = 'Não foi um desempenho legal, tente prestar mais atenção nas informações, e pesquise mais sobre o assunto.';
+            feedback = 'Foi um mal desempenho, tente rever e prestar mais atenção nas informações, e pesquise mais sobre o assunto.';
         }else if(porc_acertos <= 50){
-            feedback = 'Foi um desempenho legal, mas você pode melhorar mais, se atente melhor as informações e procure pesquisar mais sobre o assunto.';
+            feedback = 'Não foi um desempenho legal, mas você pode melhorar mais, se atente melhor as informações e procure pesquisar mais sobre o assunto.';
         }else if(porc_acertos <= 70){
-            feedback = 'Bom desempenho! Se puder rever as informações e puder entender melhor, será ótimo!';
+            feedback = 'Desempenho regular! Se puder rever as informações e puder entender melhor, será ótimo!';
         }else if(porc_acertos <= 95){
-            feedback = 'Foi quase lá!!! Você demonstra boa comprensão sobre segurança digital, continue com seu estudo se tornar fera no assunto.';
+            feedback = 'Ótimo desempenho!!! Você demonstra boa comprensão sobre segurança digital, continue com seu estudo se tornar fera no assunto.';
         } else if(porc_acertos <= 100){
             feedback = 'Você está CRAQUE na segurança digital, compreende muito bem os conceitos e informações, não deixe de estudar e assim sempre se atualize no assunto.';
         }
 
         $('.body').append('<div class="box-write p-2 pt-4 pb-4 text-center"><h3>Seu acerto foi de '+porc_acertos+'%</h3><br/><h6>'+feedback+'</h6></div>');
         $('.body').append('<br/>')
-
         window.scroll({
-            top: 1000,
+            top: 5000,
             left: 0,
             behavior: "smooth",
           });
